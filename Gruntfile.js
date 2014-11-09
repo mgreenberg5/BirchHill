1918855df7cbc2fd01adf942ad62a0d34615ce39
Michael Greenberg <mgreenberg@tonicdesign.com>
2014-11-09 15:11:29 -0500

    Setup Project

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,8 +67,8 @@ module.exports = function(grunt) {
         },
         files: {
           "bin/javascripts/main.js": [
-            "src/javascripts/lib/*.coffee",
-            "src/javascripts/lib/prequalification/**/*.coffee"
+            "src/javascripts/*.coffee",
+            "src/javascripts/prequalification/**/*.coffee"
           ]
         }
       }
